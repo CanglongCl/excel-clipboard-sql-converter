d5dda063b81632f7ed22592a52dff630d3d08c89
Lava <34743145+CanglongCl@users.noreply.github.com>
2023-07-19 23:43:07 +0800

    ready for deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
   const [origin, setOrigin] = useState<string>("");
   const [result, setResult] = useState<string>("");
 
-  const convert = () => {
+  const convert = (ignoreHeader: boolean) => {
     const table = fromTextToTable(origin);
     console.log(table);
     if (table) {
-      setResult(toSql(table, true));
+      setResult(toSql(table, ignoreHeader));
     }
   };
 
@@ -20,23 +20,28 @@ export default function Home() {
         <h1 className="text-5xl">Excel / SQL 表格转换</h1>
         <div>
           <p>
-            使用方法：复制Excel（或金山表格），粘贴在上面的文本框中，点击「转换」。然后复制下方生成的内容，将其作为一个表来用（WITH或者直接JOIN他）。
+            使用方法：复制Excel（或金山表格）的对应内容（需要带表头），粘贴在上面的文本框中，点击「转换」。然后复制下方生成的内容，将其作为一个表来用（WITH或者直接JOIN他）。
           </p>
           <p>
-            转换的内容仅支持Presto查询（如需其他查询，请将AS后的内容改名为非中文字符，并删除双引号）
+            不忽略表头转换仅支持Presto，忽略表头转换支持Spark和Presto。Hive无法使用VALUES语句，无法使用。
           </p>
         </div>
-
         <textarea
           value={origin}
           onChange={(e) => setOrigin(e.target.value)}
           className="h-40 resize-y border rounded-lg p-2 focus:outline-none focus:ring focus:border-blue-300"
         />
         <button
-          onClick={convert}
+          onClick={() => convert(false)}
           className="px-4 py-2 mt-2 bg-blue-500 hover:bg-blue-600 text-white rounded-lg focus:outline-none focus:ring focus:border-blue-300"
         >
-          convert
+          转换（带表头）
+        </button>
+        <button
+          onClick={() => convert(true)}
+          className="px-4 py-2 mt-2 bg-blue-500 hover:bg-blue-600 text-white rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+        >
+          转换（忽略表头）
         </button>
         <textarea
           value={result}
@@ -67,22 +72,21 @@ function fromTextToTable(origin: string) {
   alert("粘贴的表格信息不正确");
 }
 
-function toSql(input: Array<Array<string>>, hasHeader: boolean): string {
-  let header: string[];
-  if (hasHeader) {
-    header = input.shift() as Array<string>;
-    console.log(header);
-  } else {
+function toSql(input: Array<Array<string>>, renameHeader: boolean): string {
+  let header: string[] = input.shift() as Array<string>;
+  if (renameHeader) {
     header = Array<string>(input[0].length)
       .fill("")
-      .map((_, i) => "i");
+      .map((_, i) => `col${i + 1}`);
   }
 
   return `SELECT * FROM
     (
       VALUES
       ( ${input.map(sqlRowMapping).join("), \n(")})
-    ) AS your_table_name ("${header.join('", "')}")
+    ) AS your_table_name (${renameHeader ? "" : '"'}${header.join(
+    renameHeader ? ", " : '", "'
+  )}${renameHeader ? "" : '"'})
   `;
 }
 
